b748d881d29e9459d0820dba732be67ef69631af
chasati@microsoft.com <chasati@microsoft.com>
2021-03-06 16:02:25 +0530

    Added logout

--- Login_flash_screen/app/src/main/java/com/eduplus/eduplus_app/MainActivity.java
+++ Login_flash_screen/app/src/main/java/com/eduplus/eduplus_app/MainActivity.java
@@ -1,11 +1,18 @@
 package com.eduplus.eduplus_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +33,22 @@ public class MainActivity extends AppCompatActivity {
         rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
         handler.postDelayed(runnable, 2000);
 
+        Button buttonSignOut = findViewById(R.id.button2);
+        buttonSignOut.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+    }
+
+    public void signOut() {
+
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        startActivity(new Intent(MainActivity.this, SignInActivity.class));
+                    }
+                });
     }
 }
