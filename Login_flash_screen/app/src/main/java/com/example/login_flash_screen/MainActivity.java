fee669165d59c016836f00fe38c814b7fe7012b9
Agam <agam.jn30@gmail.com>
2021-03-14 12:43:08 +0530

    Integrated Login and Dashboard

--- Login_flash_screen/app/src/main/java/com/example/login_flash_screen/MainActivity.java
+++ Login_flash_screen/app/src/main/java/com/example/login_flash_screen/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.login_flash_screen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
     RelativeLayout rellay1, rellay2;
     Handler handler = new Handler();
     Runnable runnable = new Runnable() {
@@ -26,5 +34,15 @@ public class MainActivity extends AppCompatActivity {
         rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
         handler.postDelayed(runnable, 2000);
 
+        button = (Button) findViewById(R.id.loginbtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
---
+++ Login_flash_screen/app/src/main/java/com/example/login_flash_screen/Programming.java
@@ -0,0 +1,14 @@
+package com.example.login_flash_screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Programming extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_programming);
+    }
+}
