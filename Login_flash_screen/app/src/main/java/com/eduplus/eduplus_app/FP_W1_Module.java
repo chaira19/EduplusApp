374a232c86a7347be1f8b4132216e9310c2b602f
Agam Jain <agamjain@qti.qualcomm.com>
2021-03-24 22:33:23 +0530

    content changes

--- Login_flash_screen/app/src/main/java/com/eduplus/eduplus_app/FP_W1_Module.java
+++ Login_flash_screen/app/src/main/java/com/eduplus/eduplus_app/FP_W1_Module.java
@@ -4,14 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.WindowManager;
+
+import com.github.barteksc.pdfviewer.PDFView;
 
 public class FP_W1_Module extends AppCompatActivity {
+
+
+   // PDFView m1pdf;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_f_p__w1__module);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+      //  m1pdf = (PDFView) findViewById(R.id.pdf1);
+      //  m1pdf.fromAsset("Week1Module.pdf").load();
+
     }
 }
