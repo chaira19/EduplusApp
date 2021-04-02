5370801a2da0f73a6c85897290f865f1a28ed2ad
Agam Jain <agamjain@qti.qualcomm.com>
2021-04-02 17:46:14 +0530

    Updated content navigation

---
+++ Login_flash_screen/app/src/main/java/com/eduplus/eduplus_app/ProgrammingContent.java
@@ -0,0 +1,14 @@
+package com.eduplus.eduplus_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProgrammingContent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_programming_content);
+    }
+}
---
+++ Login_flash_screen/app/src/main/java/com/eduplus/eduplus_app/SkillContent.java
@@ -0,0 +1,49 @@
+package com.eduplus.eduplus_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class SkillContent extends AppCompatActivity implements View.OnClickListener  {
+
+    ImageView np1, np2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skill_content);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        np1 = (ImageView) findViewById(R.id.np1);
+        np2 = (ImageView) findViewById(R.id.np2);
+
+        np1.setOnClickListener(this);
+        np2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent i;
+
+        switch (v.getId()){
+            case R.id.np1:
+                i = new Intent(SkillContent.this, Skill.class);
+                startActivity(i);
+                break;
+
+            case R.id.np2:
+                i = new Intent(SkillContent.this, SkillAI.class);
+                startActivity(i);
+                break;
+
+
+        }
+    }
+}
