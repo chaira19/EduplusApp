ca58e7f522511d83ac8ce9af6a7d135f9d443d59
chasati@microsoft.com <chasati@microsoft.com>
2021-04-04 14:44:55 +0530

    Added progress card

--- Login_flash_screen/app/src/main/java/com/eduplus/eduplus_app/ProgrammingContent.java
+++ Login_flash_screen/app/src/main/java/com/eduplus/eduplus_app/ProgrammingContent.java
@@ -2,13 +2,47 @@ package com.eduplus.eduplus_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
-public class ProgrammingContent extends AppCompatActivity {
+public class ProgrammingContent extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView np1, np2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_programming_content);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        np1 = (ImageView) findViewById(R.id.clickView1);
+        np2 = (ImageView) findViewById(R.id.clickView2);
+
+        np1.setOnClickListener(this);
+        np2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent i;
+
+        switch (v.getId()){
+            case R.id.clickView1:
+                i = new Intent(ProgrammingContent.this, ProgrammingMonth1Activity.class);
+                startActivity(i);
+                break;
+
+            case R.id.clickView2:
+                i = new Intent(ProgrammingContent.this, SkillAI.class);
+                startActivity(i);
+                break;
+
+
+        }
     }
 }
--- Login_flash_screen/app/src/main/java/com/eduplus/eduplus_app/ProgressDashboard.java
+++ Login_flash_screen/app/src/main/java/com/eduplus/eduplus_app/ProgressDashboard.java
@@ -3,8 +3,12 @@ package com.eduplus.eduplus_app;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -14,6 +18,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.Map;
 
@@ -24,20 +30,60 @@ public class ProgressDashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_dashboard);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        //FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        setUserData();
 
-        //db.collection("Users").document(user.getPhoneNumber())
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        db.collection("Users").document(user.getPhoneNumber());
         db.collection("Users").document("+919410571687")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                         DocumentSnapshot doc = task.getResult();
-                        Map<String, Object> finPlanProg = (Map<String, Object>)((Map<String, Object>)doc.get("Skills")).get("FinancialPlanning");
 
-                        final ProgressBar simpleProgressBar = (ProgressBar) findViewById(R.id.progressBar1);
-                        simpleProgressBar.setProgress(getMonthProgress((Map<String, Boolean>)finPlanProg.get("Month1")));
+                        Map<String, Object> finPlanProg = (Map<String, Object>)((Map<String, Object>)doc.get("Skills")).get("FinancialPlanning");
+                        Map<String, Object> entshipProg = (Map<String, Object>)((Map<String, Object>)doc.get("Career")).get("Entrepreneurship");
+                        Map<String, Object> progProg = (Map<String, Object>)((Map<String, Object>)doc.get("Programming"));
+
+                        int progProgramming = 0;
+                        if(progProg != null)
+                        {
+                            progProgramming = getMonthProgress((Map<String, Boolean>)progProg.get("Month1"));
+                        }
+                        int progSkills = 0;
+                        if(finPlanProg != null)
+                        {
+                            progSkills = getMonthProgress((Map<String, Boolean>)finPlanProg.get("Month1"));
+                        }
+                        int progCareer = 0;
+                        if(entshipProg != null)
+                        {
+                            progCareer = getMonthProgress((Map<String, Boolean>)entshipProg.get("Month1"));
+                        }
+
+                        final ProgressBar progressBar1 = (ProgressBar) findViewById(R.id.progressBar1);
+                        progressBar1.setProgress(progProgramming);
+
+                        final ProgressBar progressBar2 = (ProgressBar) findViewById(R.id.progressBar2);
+                        progressBar2.setProgress(progSkills);
+
+                        final ProgressBar progressBar3 = (ProgressBar) findViewById(R.id.progressBar3);
+                        progressBar3.setProgress(progCareer);
+
+                        final ProgressBar overallProgress = (ProgressBar) findViewById(R.id.circular_progress);
+                        overallProgress.setProgress(progProgramming + progSkills + progCareer);
+
+                        final TextView progTV1 = (TextView) findViewById(R.id.textViewRightSubHeading1);
+                        final TextView progTV2 = (TextView) findViewById(R.id.textViewRightSubHeading2);
+                        final TextView progTV3 = (TextView) findViewById(R.id.textViewRightSubHeading3);
+                        final TextView progTV4 = (TextView) findViewById(R.id.textViewOverall);
+
+                        progTV1.setText(((progProgramming*100)/4) + "%");
+                        progTV2.setText(((progSkills*100)/4) + "%");
+                        progTV3.setText(((progCareer*100)/4) + "%");
+                        progTV4.setText(((progProgramming + progSkills + progCareer)*100)/12 + "%\n Overall Progress");
                     }
                 });
     }
@@ -53,5 +99,74 @@ public class ProgressDashboard extends AppCompatActivity {
             }
         }
         return progress;
+    }
+
+    private void setUserData() {
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        //We are creating phone numbers as userIds
+         String userId = user.getPhoneNumber();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Users").document(userId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+
+                    String name = (String) document.get("Name");
+                    String schoolName = (String) document.get("SchoolName");
+                    String photoId = (String) document.get("PhotoId");
+
+                    TextView nameTextView = findViewById(R.id.tv1);
+                    nameTextView.setText(name.toUpperCase());
+
+                    TextView schoolNameText = findViewById(R.id.tv2);
+                    schoolNameText.setText(schoolName.toUpperCase());
+
+                    TextView classTextView = findViewById(R.id.tv3);
+                    classTextView.setText("CLASS X");
+
+                    setImageInImageView(findViewById(R.id.studentImage), photoId, "userImages/");
+                }
+                else {
+                    Log.e("Error", "Task is not successful");
+                }
+
+            }
+        });
+    }
+
+    // set image from storage
+    private void setImageInImageView(final ImageView imageView, String imageId, String imageFolder)
+    {
+        if(imageId == null || imageId.isEmpty())
+        {
+//            imageView.setImageResource(R.drawable.app_logo);
+            return;
+        }
+        else
+        {
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference();
+            StorageReference imageRef = storageRef.child(imageFolder + imageId);
+
+            imageRef.getBytes(Long.MAX_VALUE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                @Override
+                public void onSuccess(byte[] bytes) {
+
+                    if(imageView != null)
+                    {
+                        imageView.setImageBitmap(BitmapFactory.decodeByteArray(bytes, 0, bytes.length));
+                        //Glide.with(imageView).load(bytes).into(imageView);
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    Log.e("Error", "Image Task is not successful");
+                }
+            });
+        }
     }
 }
