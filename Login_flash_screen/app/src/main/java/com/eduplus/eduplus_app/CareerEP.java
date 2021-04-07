310d46b747b287b7143f58c04c4408622b7a1adc
Agam Jain <agamjain@qti.qualcomm.com>
2021-04-07 20:25:32 +0530

    Color Changes

--- Login_flash_screen/app/src/main/java/com/eduplus/eduplus_app/CareerEP.java
+++ Login_flash_screen/app/src/main/java/com/eduplus/eduplus_app/CareerEP.java
@@ -49,7 +49,8 @@ public class CareerEP extends AppCompatActivity implements View.OnClickListener 
         check3 = findViewById(R.id.check3);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        String userId = user.getPhoneNumber();
+        //String userId = user.getPhoneNumber();
+        String userId = "+918006491339";
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("Users").document(userId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
--- Login_flash_screen/app/src/main/java/com/eduplus/eduplus_app/test_activity.java
+++ Login_flash_screen/app/src/main/java/com/eduplus/eduplus_app/test_activity.java
@@ -1,14 +1,19 @@
 package com.eduplus.eduplus_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class test_activity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_activity);
+
+
     }
 }
